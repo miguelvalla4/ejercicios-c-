1207eb93b59e7b9aea159932ed69b632c093ab30
Garzolio <miguelvalla4@gmail.com>
2023-02-26 01:10:25 +0100

    feat/add-print-an-empty-square

--- cuadrado.cpp
+++ cuadrado.cpp
@@ -2,16 +2,36 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
 
-    cout << "Introduce un numero: " << endl;
+    cout << "Enter the size of the square: " << endl;
     cin >> n;
 
-    for (int i=0; i<n; i++) {
-        cout << "*";
+    // print the top border
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
     }
+
+    cout << endl;
+
+    // print the sides and the middle spaces
+    for (int i = 0; i < n - 2; i++) {
+        cout << "*";
+        
+        for (int j = 0; j < n - 2; j++) {
+            cout << "  ";
+        }
+        
+        cout << " *\n";
+    }
+
+    // print the bottom border
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
+    }
+
+    cout << endl;
 
     return 0;
 }
